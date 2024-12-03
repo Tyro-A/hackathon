3d9fed7ef3b465d6d799e4916c33feeaaab7e96b
ZICKO <zicko.iq@gmail.com>
2024-12-03 10:27:12 +0300

    the most final shit

--- hackathon/admin-dashboard.php
+++ hackathon/admin-dashboard.php
@@ -132,9 +132,9 @@ $result = $conn->query($sql);
                                 if (!empty($$imageVar)) : ?>
                                     <a href="project.php?project_id=<?php echo $project_id; ?>">
                                       <img src="<?php echo $$imageVar; ?>" class="project-image w-32 h-32 object-cover rounded" alt="Project Image <?php echo $i; ?>" class="w-32 h-32 object-cover rounded">
+                                      </a>
                                       <?php else : ?>
                                       <div class=" w-15 h-32 object-cover rounded"></div>
-                                    </a>
                                   <?php endif; ?>
                                 <?php endfor; ?>
 
--- hackathon/index.php
+++ hackathon/index.php
@@ -167,7 +167,7 @@ JOIN category on (projects.cat_id=category.category_id)";
                     <a href="project.php?project_id=<?php echo $project_id; ?>">
                       <img src="<?php echo $$imageVar; ?>" class="project-image w-32 h-32 object-cover rounded" alt="Project Image <?php echo $i; ?>" class="w-32 h-32 object-cover rounded">
                       <?php else : ?>
-                      <div class=" w-15 h-32 object-cover rounded"></div>
+                      <div class=" w-15 h-32 object-cover rounded mt-4"></div>
                     </a>
                   <?php endif; ?>
                 <?php endfor; ?>
