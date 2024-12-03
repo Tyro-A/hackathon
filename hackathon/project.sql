49e6283d70dced41f81d6fddc4ec335dc05e53d1
mohammed ali <mhmd515@gmail.com>
2024-12-03 10:17:16 +0300

    sex

--- hackathon/project.sql
+++ hackathon/project.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2024 at 03:49 AM
+-- Generation Time: Dec 03, 2024 at 08:15 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -61,7 +61,10 @@ INSERT INTO `images` (`images_id`, `image_1`, `image_2`, `image_3`, `image_4`) V
 (26, 'uploads/project-1.png', 'uploads/project1b.png', 'uploads/project1c.png', 'uploads/project1d.png'),
 (28, 'uploads/project2.webp', '', '', ''),
 (29, 'uploads/project3.webp', '', '', ''),
-(30, 'uploads/project4.png', '', '', '');
+(30, 'uploads/project4.png', '', '', ''),
+(41, 'uploads/1.png', 'uploads/2.png', 'uploads/3.png', 'uploads/4.png'),
+(42, 'uploads/5355078977317889492.jpg', 'uploads/5354960818472610026.jpg', 'uploads/5355078977317889496.jpg', 'uploads/5355078977317889499.jpg'),
+(43, 'uploads/14ff128a-d898-4dd6-9227-97ff5221e33e.jpeg', 'uploads/556fbc25-03e8-4fa7-938d-2cd2cf35bb80.jpeg', 'uploads/96f3540f-0551-451c-a31f-864dabbbd9f9.jpeg', '');
 
 -- --------------------------------------------------------
 
@@ -85,7 +88,10 @@ INSERT INTO `members` (`members_id`, `name_1`, `name_2`, `name_3`, `name_4`) VAL
 (24, '', '', '', ''),
 (26, '', '', '', ''),
 (27, '', '', '', ''),
-(28, 'someone', 'someone', 'someone', 'someone');
+(28, 'someone', 'someone', 'someone', 'someone'),
+(39, 'Zainab Hazem Hassan', 'bent alhuda assad abdulcareem', '', ''),
+(40, 'Islam Amer Sharif ', ' Hanin Hassan Hadi', ' Nour Al-Hoda Louay Amin', ''),
+(41, 'Abbas Fadhel Kazim', 'Dhu al-Fiqar Abdul Hussein Jalil', 'Murtada Mohammed Abdul Redha', '');
 
 -- --------------------------------------------------------
 
@@ -117,7 +123,10 @@ INSERT INTO `projects` (`project_id`, `title_ar`, `title_en`, `supervisor`, `des
 (22, 'اداره مشاريع', 'project management ', 'Dr.Zaid ', 'to make the project management easy with this full responsive web application ', 100, '', NULL, 1, 24, 26, 10, 12),
 (24, 'استطلاع', 'survey form', 'Dr.Saif', 'take your survey in just few second with web responsive design', 100, '', NULL, 1, 26, 28, 11, 12),
 (25, 'المكتبة الإلكترونية', 'e-liberary', 'Dr.Mohammed', 'sell your book online with ', 100, '', NULL, 1, 27, 29, 12, 12),
-(26, 'الطقس', 'weather ', 'someone', 'something about weather ', 40, '', NULL, 0, 28, 30, 13, 12);
+(26, 'الطقس', 'weather ', 'someone', 'something about weather ', 40, '', NULL, 0, 28, 30, 13, 12),
+(27, 'موقع (واصل) الذكي لخطوط نقل الطلاب والموظفين', 'Wasil smart website for student and employee trans', 'Eng. Ahmed Ali Karim', 'Wasil Smart Project provides a platform to organize daily transportation for students and staff to the University of Karbala, with dedicated lines and regular schedules to ensure a comfortable and safe means of transportation.', 64, '', NULL, NULL, 39, 41, 15, 12),
+(28, 'داري ', 'Dari ', 'Dr. A.M. Mohammed Mohsen Al-Abbadi', 'At Dary, we offer an innovative real estate experience that combines modern technology and smart solutions. We seek to facilitate the search for properties through artificial intelligence and provide specialized tools.\r\nOur services include:\r\n1. VR technology to display the property with furniture.\r\n2. Virtual reality to explore properties with 360 cameras.', 34, 'Al Zarifi Real Estate Company', NULL, NULL, 40, 42, 15, 12),
+(29, 'خدمة توصيل الوقود', 'FuelMate', 'Mr. Ali Mohammed Reda', 'Project Idea: A reliable fuel delivery service in Iraq, delivering fuel directly to the customer’s doorstep quickly and easily. With a focus on safety and customer satisfaction, the service aims to save users time and effort, while ensuring peace of mind. It is the ideal choice for convenience and reliability in fuel delivery.', 40, '', NULL, NULL, 41, 43, 15, 12);
 
 -- --------------------------------------------------------
 
@@ -149,7 +158,8 @@ INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `phone`, `co
 (11, 'Ali', 'Sami', 'zicko@gmail.com', '2', 'CS & IT', 'IT', 0, '0000-00-00', '1234', NULL),
 (12, 'Mohammed', 'Ali', 'Mohammed@gmail.com', '3', 'CS & IT', 'IT', 0, '0000-00-00', '1234', NULL),
 (13, 'some', 'one', 'someone@gmail.com', '5', 'CS & IT', 'IT', 0, '0000-00-00', '1234', NULL),
-(14, 'ali', '', 'ali@gmail.com', '6', 'CS & IT', 'IT', 0, '0000-00-00', '1234', NULL);
+(14, 'ali', '', 'ali@gmail.com', '6', 'CS & IT', 'IT', 0, '0000-00-00', '1234', NULL),
+(15, 'Zainab', 'Hazem', 'z@gmail.com', '07711111111', 'cs&it', 'cs', 0, '0000-00-00', '1234', NULL);
 
 --
 -- Indexes for dumped tables
@@ -204,25 +214,25 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `images_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `images_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `members_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `members_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
